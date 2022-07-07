75b7ec0c1662044a09904475fba97362ac78a96e
Mehmet Fatih Erdem <mehmeterdem1024@gmail.com>
2022-07-07 23:00:20 +0300

    upload images to cloud

--- src/routes/CSVImport.ts
+++ src/routes/CSVImport.ts
@@ -2,6 +2,6 @@ import express from 'express';
 import { importCSV } from '../controllers/CSVImport';
 const router = express.Router();
 
-router.get('/import', importCSV);
+router.post('/import', importCSV);
 
 export default router;
