3fd2bab97366aef451d4b707efdb9fb98c4dc1b5
Mehmet Fatih Erdem <mehmeterdem1024@gmail.com>
2022-07-10 11:33:47 +0300

    add jwt auth to admin login

--- src/controllers/Admin.ts
+++ src/controllers/Admin.ts
@@ -1,6 +1,7 @@
 import bcrypt from 'bcrypt';
 import { Request, Response } from 'express';
 import User from '../models/User';
+import jwt from 'jsonwebtoken';
 
 const loginAdmin = async (req: Request, res: Response) => {
   const { email, password } = req.body;
@@ -10,6 +11,23 @@ const loginAdmin = async (req: Request, res: Response) => {
 
     const match = await bcrypt.compare(password, admin.password);
     if (!match) throw new Error('wrong password');
+
+    const cookieAge = 24 * 3600; // Default cookie expiry time is 1 day
+
+    const token = await jwt.sign(
+      {
+        name: admin.name,
+        lastName: admin.lastName,
+        email: admin.lastName,
+      },
+      process.env.JWT_SECRET,
+      { expiresIn: cookieAge }
+    );
+
+    res.cookie('_t', token, {
+      maxAge: cookieAge,
+      signed: true,
+    });
 
     res.json({ message: admin.serializedForLogin() });
   } catch (err) {
