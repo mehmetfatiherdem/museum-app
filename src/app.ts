64b7fe064c9837fbc6a0d2079374adf8186ce60f
Mehmet Fatih Erdem <mehmeterdem1024@gmail.com>
2022-07-17 17:16:06 +0300

    fix view dir

--- src/app.ts
+++ src/app.ts
@@ -8,20 +8,21 @@ import cookieParser from 'cookie-parser';
 import swaggerUi from 'swagger-ui-express';
 import * as swaggerDocument from '../swagger.json';
 import cors from 'cors';
+import path from 'path';
 
 app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors());
-app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 
 app.get('/google-sign', function (req, res) {
-  res.render(`${__dirname}/../src/views/auth.ejs`);
+  res.render(`auth.ejs`);
 });
 
 app.use('/api', routes);
 
- app.use('/', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use('/', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.listen(port, () => {
   console.log(`Express is listening at http://localhost:${port}`);
